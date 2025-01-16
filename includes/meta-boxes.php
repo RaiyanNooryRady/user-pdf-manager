389a15bfb4bc0453a1f1fd4d40eb638d53fcd9e7
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-01-16 16:08:31 +0600

    pdf link

--- includes/meta-boxes.php
+++ includes/meta-boxes.php
@@ -7,18 +7,21 @@ add_action('add_meta_boxes', function () {
 
 function render_pdf_meta_box($post)
 {
-    $pdf_link = get_post_meta($post->ID, 'pdf_link', true);
-    $pdf_link2= get_post_meta($post->ID,'pdf_link2', true);
-    $pdf_link3= get_post_meta($post->ID,'pdf_link3', true);
     $username = get_post_meta($post->ID, 'username', true);
+    // $pdf_link = get_post_meta($post->ID, 'pdf_link1', true);
+    for ($i = 1; $i <= 10; $i++) {
+        ${"pdf_link$i"} = get_post_meta($post->ID, "pdf_link$i", true);
+    }
+
+
     ?>
     <p>
         <label for="username">Username:</label><br>
         <input type="text" name="username" id="username" value="<?php echo esc_attr($username); ?>" style="width:100%;">
     </p>
     <p>
-        <label for="pdf_link">PDF URL 1:</label><br>
-        <input type="text" name="pdf_link" id="pdf_link" value="<?php echo esc_attr($pdf_link); ?>" style="width:100%;">
+        <label for="pdf_link1">PDF URL 1:</label><br>
+        <input type="text" name="pdf_link1" id="pdf_link1" value="<?php echo esc_attr($pdf_link1); ?>" style="width:100%;">
     </p>
     <p>
         <label for="pdf_link2">PDF URL 2:</label><br>
@@ -28,7 +31,7 @@ function render_pdf_meta_box($post)
         <label for="pdf_link3">PDF URL 3:</label><br>
         <input type="text" name="pdf_link3" id="pdf_link3" value="<?php echo esc_attr($pdf_link3); ?>" style="width:100%;">
     </p>
-    
+
     <?php
 }
 
@@ -37,8 +40,8 @@ add_action('save_post', function ($post_id) {
     if (isset($_POST['username'])) {
         update_post_meta($post_id, 'username', sanitize_text_field($_POST['username']));
     }
-    if (isset($_POST['pdf_link'])) {
-        update_post_meta($post_id, 'pdf_link', sanitize_text_field($_POST['pdf_link']));
+    if (isset($_POST['pdf_link1'])) {
+        update_post_meta($post_id, 'pdf_link1', sanitize_text_field($_POST['pdf_link1']));
     }
     if (isset($_POST['pdf_link2'])) {
         update_post_meta($post_id, 'pdf_link2', sanitize_text_field($_POST['pdf_link2']));
@@ -46,5 +49,5 @@ add_action('save_post', function ($post_id) {
     if (isset($_POST['pdf_link3'])) {
         update_post_meta($post_id, 'pdf_link3', sanitize_text_field($_POST['pdf_link3']));
     }
-    
+
 });
