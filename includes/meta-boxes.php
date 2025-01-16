6f470079f9af38a1e0a6cfb4a806e459f38f92e8
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-01-16 18:12:42 +0600

    updated custom meta fields with for loop

--- includes/meta-boxes.php
+++ includes/meta-boxes.php
@@ -12,26 +12,23 @@ function render_pdf_meta_box($post)
     for ($i = 1; $i <= 10; $i++) {
         ${"pdf_link$i"} = get_post_meta($post->ID, "pdf_link$i", true);
     }
-
-
     ?>
     <p>
         <label for="username">Username:</label><br>
         <input type="text" name="username" id="username" value="<?php echo esc_attr($username); ?>" style="width:100%;">
     </p>
-    <p>
-        <label for="pdf_link1">PDF URL 1:</label><br>
-        <input type="text" name="pdf_link1" id="pdf_link1" value="<?php echo esc_attr($pdf_link1); ?>" style="width:100%;">
-    </p>
-    <p>
-        <label for="pdf_link2">PDF URL 2:</label><br>
-        <input type="text" name="pdf_link2" id="pdf_link2" value="<?php echo esc_attr($pdf_link2); ?>" style="width:100%;">
-    </p>
-    <p>
-        <label for="pdf_link3">PDF URL 3:</label><br>
-        <input type="text" name="pdf_link3" id="pdf_link3" value="<?php echo esc_attr($pdf_link3); ?>" style="width:100%;">
-    </p>
+    <?php
+    //Displaying the custom meta fields in dashboard
+    for ($i = 1; $i <= 10; $i++) {
+        ?>
+        <p>
+            <label for="pdf_link<?php echo $i; ?>">PDF URL <?php echo $i; ?>:</label><br>
+            <input type="text" name="pdf_link<?php echo $i; ?>" id="pdf_link<?php echo $i; ?>" value="<?php echo esc_attr(${"pdf_link$i"}); ?>" style="width:100%;">
+        </p>
 
+        <?php
+    }
+    ?>
     <?php
 }
 
@@ -40,14 +37,15 @@ add_action('save_post', function ($post_id) {
     if (isset($_POST['username'])) {
         update_post_meta($post_id, 'username', sanitize_text_field($_POST['username']));
     }
-    if (isset($_POST['pdf_link1'])) {
-        update_post_meta($post_id, 'pdf_link1', sanitize_text_field($_POST['pdf_link1']));
+    
+    for ($i = 1; $i <= 10; $i++) {
+        // Generate the field name dynamically
+        $field_name = 'pdf_link' . $i;
+    
+        // Check if the field is set in $_POST
+        if (isset($_POST[$field_name])) {
+            // Update the corresponding post meta
+            update_post_meta($post_id, $field_name, sanitize_text_field($_POST[$field_name]));
+        }
     }
-    if (isset($_POST['pdf_link2'])) {
-        update_post_meta($post_id, 'pdf_link2', sanitize_text_field($_POST['pdf_link2']));
-    }
-    if (isset($_POST['pdf_link3'])) {
-        update_post_meta($post_id, 'pdf_link3', sanitize_text_field($_POST['pdf_link3']));
-    }
-
 });
