35cfa17dda5e8aeb1ab4b2a3ba6036c32ee0c10f
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-01-20 17:39:42 +0600

    updated conditon

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -42,9 +42,12 @@ add_shortcode('user_pdfs', function () {
 				<div class="card shadow">
 					<h5 class="policy-header">Current & Upcoming</h5>
 					<div class="no-policy text-center py-5 rounded-4">
-						<img src="<?php echo plugin_dir_url(__FILE__) . '../assets/images/no-policy.svg'; ?>"class="img-no-policy"
+						<img src="<?php echo plugin_dir_url(__FILE__) . '../assets/images/no-policy.svg'; ?>" class="img-no-policy"
 							alt="">
 						<h4 class="text-light">You have no active temporary policies</h4>
+						<div class="text-light text-center py-3">
+							<a href="#" class="p-3 btn-get-quote text-decoration-none">Get a quote</a>
+						</div>
 					</div>
 					<h5 class="policy-header">Past policies</h5>
 				</div>
