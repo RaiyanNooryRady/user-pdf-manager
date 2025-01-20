9d57b3784bb6cd57e22810e2099d6207f28eb55c
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-01-20 13:03:04 +0600

    added before pdf header

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -32,7 +32,9 @@ add_shortcode('user_pdfs', function () {
 
 			
 			?>
-			<h2 class="text-center text-danger">Hello Check</h2>
+			
+			<div class="before-pdf-header text-light text-center py-3">As a tempcover member enjoy a discount on policies lasting one day or more</div>
+
 			<div style="text-align: center; margin-bottom: 20px;">
 				<button id="view-documents-btn"
 					style="padding: 10px 20px; font-size: 16px; background-color: transparent; font-weight:600px; color: #264694; border: 2px solid #264694; border-radius: 8px; cursor: pointer;">
