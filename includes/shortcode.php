94f4ad51b2e3574ee850cc4f78764a1f48a633e6
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-01-20 17:31:21 +0600

    designed no policy page

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -41,6 +41,11 @@ add_shortcode('user_pdfs', function () {
 			<div class="upm-not-assigned-pdf my-3">
 				<div class="card shadow">
 					<h5 class="policy-header">Current & Upcoming</h5>
+					<div class="no-policy text-center py-5 rounded-4">
+						<img src="<?php echo plugin_dir_url(__FILE__) . '../assets/images/no-policy.svg'; ?>"class="img-no-policy"
+							alt="">
+						<h4 class="text-light">You have no active temporary policies</h4>
+					</div>
 					<h5 class="policy-header">Past policies</h5>
 				</div>
 
