e6cd1a1ebe7b9d867895083d3bd11333c3e61125
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-02-03 15:04:16 +0600

    removed hard coded texts

--- includes/template-parts/hello-author.php
+++ includes/template-parts/hello-author.php
@@ -1,8 +1,9 @@
-<div class="before-pdf-header text-light text-center py-3 mb-3">As a tempcover member enjoy a discount on policies
+<div class="before-pdf-header text-light text-center py-3 mb-3"><?Php esc_html_e('As a tempcover member enjoy a discount on policies
     lasting
-    one day or more</div>
+    one day or more', 'user-pdf-manager'); ?></div>
 <div class="text-light text-center py-3 mb-3">
-    <a href="https://www.tempcover.com/start-quote" target="_blank" class="btn-get-quote text-decoration-none">GET A QUOTE</a>
+    <a href="https://www.tempcover.com/start-quote" target="_blank"
+        class="btn-get-quote text-decoration-none"><?php esc_html_e('GET A QUOTE', 'user-pdf-manager'); ?></a>
 </div>
 
-<h2 class="upm-author">Hello <?php echo esc_html($current_user->display_name) ?></h2>
+<h2 class="upm-author"><?php esc_html_e('Hello ', 'user-pdf-manager'); echo esc_html($current_user->display_name); ?></h2>
--- includes/template-parts/not-assigned-pdf.php
+++ includes/template-parts/not-assigned-pdf.php
@@ -1,17 +1,16 @@
 <div class="upm-not-assigned-pdf my-3">
     <div class="card shadow-sm">
-        <h5 class="policy-header">Current & Upcoming</h5>
+        <h5 class="policy-header mb-4"><?php esc_html_e('Current & Upcoming','user-pdf-manager'); ?></h5>
         <div class="no-policy text-center py-5 rounded-4">
             <img src="<?php echo plugins_url() . '/user-pdf-manager/assets/images/no-policy.svg'; ?>"
                 class="img-no-policy" alt="no-policy">
 
-            <h4 class="text-light">You have no active temporary policies</h4>
+            <h4 class="text-light"><?php esc_html_e('You have no active temporary policies','user-pdf-manager'); ?></h4>
             <div class="text-light text-center py-3">
-                <a href="https://www.tempcover.com/start-quote" target="_blank" class="p-3 btn-get-quote text-decoration-none">Get a quote</a>
+                <a href="https://www.tempcover.com/start-quote" target="_blank" class="p-3 btn-get-quote text-decoration-none"><?php esc_html_e('Get a quote', 'user-pdf-manager');?></a>
             </div>
         </div>
-        <h5 class="policy-header">Past policies</h5>
+        <h5 class="policy-header mt-3"><?php esc_html_e('Past Policies', 'user-pdf-manager'); ?></h5>
     </div>
 
 </div>
-<?php //echo plugin_dir_url(__FILE__) . 'assets/images/no-policy.svg'; ?>
