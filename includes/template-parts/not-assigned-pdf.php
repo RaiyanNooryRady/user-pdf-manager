5aed5649c9eb28614401e35e90cca7c6f1b50f2e
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-02-13 16:13:25 +0600

    removed no policy images

--- includes/template-parts/not-assigned-pdf.php
+++ includes/template-parts/not-assigned-pdf.php
@@ -7,9 +7,6 @@ $button_link = get_option('user_pdf_manager_button_link', '#');
     <div class="card shadow-sm">
         <h5 class="policy-header mb-4"><?php esc_html_e('Current & Upcoming', 'user-pdf-manager'); ?></h5>
         <div class="no-policy text-center py-5 rounded-4">
-            <img src="<?php echo plugins_url() . '/user-pdf-manager/assets/images/no-policy.svg'; ?>"
-                class="img-no-policy" alt="no-policy">
-
             <h4 class="text-light"><?php esc_html_e('You have no active temporary policies', 'user-pdf-manager'); ?></h4>
             <div class="text-light text-center py-3">
                 <a href="<?php echo esc_url($button_link); ?>" target="_blank"
